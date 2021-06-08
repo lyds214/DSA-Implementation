f2105c729db9f3fd2a899748ee5cf31ad87ddd61
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2021-06-08 13:15:31 -0700

    Factorial

---
+++ Recursion/Factorial.py
@@ -0,0 +1,15 @@
+def factorialRecursion(n):
+    if n <= 0:
+        return 
+    if n == 1:
+        return n
+    
+    return n * factorialRecursion(n - 1)
+
+def factorialIterative(n):
+    fact = 1 
+
+    for num in range(2, n - 1):
+        fact += num 
+    
+    return fact
